8179d03d393c3971f6c32958d4769f719070388f
João Emanuel <joao.e.m95@gmail.com>
2022-04-11 22:37:34 -0300

    Add canvas.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <FirstPage />
+  <FirstPage/>
 </template>
 
 <script>
--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -1,10 +1,49 @@
 <template>
-    <h1>Olá Mundo!</h1>
+      <div>{{ message}}</div>
+  <!-- Canvas -->
+  <canvas id="c"></canvas>
+  <!-- Add Rectangle Button -->
+  <div>
+    <button @click="drawRect">Add Rect</button>
+    <button @click="subWidth">-</button>
+    <button @click="addWidth">+</button>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'FirstPage'
+    name: 'FirstPage',
+    data() {
+        return {
+            message: 'Vue + Canvas API',
+            vueCanvas: null,
+            rectWidth:200
+        }
+    },
+    mounted() {
+        var c = document.getElementById("c");
+        var ctx = c.getContext("2d");
+        this.vueCanvas = ctx;
+    },
+    methods: {
+        drawRect() {
+            // limpa a tela 
+            this.vueCanvas.clearRect(0, 0, 400, 200);
+            // desenha rect 
+            this.vueCanvas.beginPath();
+            this.vueCanvas.rect(20, 20, this.rectWidth, 100);
+            this.vueCanvas.stroke();
+        },
+        addWidth() {
+            this.rectWidth += 10
+            this.drawRect()
+        },
+        subWidth() {
+            this.rectWidth -= 10
+            this.drawRect()
+        }
+    }
+
 }
 </script>
 

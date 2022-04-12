84bca2bb037a9f68c5ce48da365bc90c0345a0df
João Emanuel <joao.e.m95@gmail.com>
2022-04-11 22:02:32 -0300

    Basics configuration.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <FirstPage />
+</template>
+
+<script>
+import FirstPage from "./components/FirstPage.vue"
+
+export default {
+  name: 'App',
+  components:{
+    FirstPage
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/FirstPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Olá Mundo!</h1>
+</template>
+
+<script>
+export default {
+    name: 'FirstPage'
+}
+</script>
+
+<style>
+</style>
